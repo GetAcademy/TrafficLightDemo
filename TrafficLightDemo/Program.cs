603d5aed0e0ae9128e6fa45016a5135fbfab7635
GET Academy <terje@getacademy.no>
2023-01-24 11:08:40 +0100

    x

--- TrafficLightDemo/Program.cs
+++ TrafficLightDemo/Program.cs
@@ -11,3 +11,18 @@ trafficLight.Show();
 trafficLight.Next();
 trafficLight.Show();
 
+
+
+
+//var phase  = TrafficLightPhases.Red;
+//if (phase == TrafficLightPhases.Yellow)
+//{
+//    phase = TrafficLightPhases.Red;
+//}
+//else
+//{
+//    phase++;
+//}
+//var phase3 = (TrafficLightPhases)3;
+//Console.WriteLine(phase.ToString());
+
---
+++ TrafficLightDemo/TrafficLightPhases.cs
@@ -0,0 +1,10 @@
+﻿namespace TrafficLightDemo
+{
+    enum TrafficLightPhases
+    {
+        Red = 1,
+        RedAndYellow = 2,
+        Yellow = 3,
+        Green = 4,
+    }
+}
